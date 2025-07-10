1c2fb559977d682c59b576cf735377150e225402
Villon CHEN <villon.chen@oppida.fr>
2025-07-10 15:46:48 +0200

    Fix missing args

--- sastb.sh
+++ sastb.sh
@@ -4,4 +4,4 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Run CLI
-$SCRIPT_DIR/.venv/bin/python3 $SCRIPT_DIR/main.py
+$SCRIPT_DIR/.venv/bin/python3 $SCRIPT_DIR/main.py "$@"
