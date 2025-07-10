74e1331f2e0427963faf2952d485ad3a55687be3
Villon CHEN <villon.chen@oppida.fr>
2025-07-10 16:11:00 +0200

    Use default rules for Coverity analysis

--- SASTs/Coverity/analyzer.py
+++ SASTs/Coverity/analyzer.py
@@ -37,8 +37,7 @@ def run_single_project_buildless(lang, project_dir, result_dir):
 
     # Support Java only
     # TODO: C/C++, support config flag -co
-    full_analyze_command = "cov-analyze --dir idir --enable-callgraph-metrics".split(" ") + ALL_SECURITY + SPOTBUGS + JAVA_CHECKERS_CRITIQUES + JAVA_CHECKERS_IMPORTANTS + JAVA_CHECKERS_MODERES
-    _ = run_command(" ".join(full_analyze_command), project_dir)
+    _ = run_command("cov-analyze --dir idir --all-security --enable-callgraph-metrics", project_dir)
     end = time.time()
     click.echo(f"Time taken (capture + analysis): {timedelta(seconds=end-start)}")
 
@@ -47,6 +46,7 @@ def run_single_project_buildless(lang, project_dir, result_dir):
 ## Datasets
 import datasets.CVEfixes.helper as CVEfixes
 import datasets.SemgrepTest.helper as SemgrepTest
+import datasets.BenchmarkJava.helper as BenchmarkJava
 
 
 # TODO: max repo size
@@ -117,8 +117,35 @@ def run_SemgrepTest(lang, overwrite=False):
     # Clear temporary directory
     temp_dir.cleanup()
 
+def run_BenchmarkJava(overwrite=False):
+    testcodes = BenchmarkJava.load_dataset()
+
+    result_path = BenchmarkJava_RESULT_DIR
+    os.makedirs(result_path, exist_ok=True)
+
+    if os.path.isdir(result_path):
+        if os.listdir(result_path) and not overwrite:
+            click.echo("Results already exist, please use --overwrite to delete old results")
+            return
+
+    # Create temporary directory for the project
+    temp_dir = tempfile.TemporaryDirectory()
+    temp_path = temp_dir.name
+
+    # Copy test files into the temporary directory
+    for testcode in testcodes:
+        testcode.save(temp_path)
+
+    # Run analysis
+    run_single_project_buildless("java", temp_path, result_path)
+
+    # Clear temporary directory
+    temp_dir.cleanup()
+
+
 def list_all_datasets():
     all_datasets = []
     all_datasets.extend(CVEfixes.list_dataset())
     all_datasets.extend(SemgrepTest.list_dataset())
+    all_datasets.extend(BenchmarkJava.list_dataset())
     return all_datasets
