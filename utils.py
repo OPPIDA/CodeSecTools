be7c2d00b5d73687b4c01f7c8f12b21c53895278
Villon CHEN <villon.chen@oppida.fr>
2025-08-05 17:42:06 +0200

    Add global exception handler

--- utils.py
+++ utils.py
@@ -1,62 +1,27 @@
-import csv
-import glob
-import json
 import os
-import re
-import shutil
 import subprocess
 import sys
-import tempfile
-import time
-import xml
-from datetime import timedelta
+import traceback
+from types import TracebackType
 
 import click
-import git
-import humanize
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
-import requests
-import xmltodict
-import yaml
 
-# Matplotlib config
-matplotlib.use("pgf")
-matplotlib.rcParams.update(
-    {
-        "pgf.texsystem": "pdflatex",
-        "font.family": "serif",
-        "font.size": 11,
-        "text.usetex": True,
-        "pgf.rcfonts": False,
-    }
-)
-
-# Differentiate project directory and this tool directory
-WORKING_DIR = os.getcwd()
-if sys.path[0]:
-    os.chdir(sys.path[0])
+# Use current working directory
+WORKING_DIR = os.environ.get("WORKING_DIR", "")
+if not WORKING_DIR:
+    click.echo("Fatal error: WORKING_DIR is not defined!", err=True)
+    sys.exit(1)
 
 
-# Display CAT output
-def SILENT():
-    return os.environ.get("SILENT", "0") == "1"
-
-
-# Import CWE
-CWE = {}
-for file_path in glob.glob(os.path.join("data", "CWE_*.csv")):
-    with open(file_path, mode="r", encoding="utf-8") as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            CWE[int(row["CWE-ID"])] = row
+# Debugging
+def DEBUG() -> bool:
+    return os.environ.get("DEBUG", "0") == "1"
 
 
 # Subprocess wrapper
-def run_command(command: str, cwd):
+def run_command(command: list[str], cwd: str) -> tuple[int | None, str]:
     process = subprocess.Popen(
-        command.split(" "),
+        command,
         cwd=cwd,
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
@@ -66,9 +31,57 @@ def run_command(command: str, cwd):
 
     stdout = ""
 
-    for line in process.stdout:
-        stdout += line
-        if not SILENT():
-            print(line, end="")
+    if process.stdout:
+        for line in process.stdout:
+            stdout += line
+            if DEBUG():
+                click.echo(line, nl=False)
 
-    return stdout
+    retcode = process.poll()
+
+    return (retcode, stdout)
+
+
+# Custom Exceptions
+class MissingFile(Exception):
+    pass
+
+
+class NonZeroExit(Exception):
+    pass
+
+
+# Global error handler
+def global_excepthook(
+    exc_type: type[BaseException],
+    exc_value: BaseException,
+    exc_traceback: TracebackType | None,
+) -> None:
+    if DEBUG():
+        traceback.print_exception(exc_type, exc_value, exc_traceback)
+
+    if issubclass(exc_type, MissingFile):
+        files = exc_value.args[0]
+        match len(files):
+            case 1:
+                click.echo(
+                    f"[ERROR] File not found: {click.style(files[0], fg='red', bold=True)}",
+                    err=True,
+                )
+            case _:
+                click.echo(
+                    f"[ERROR] Files not found: {click.style(', '.join(files), fg='red', bold=True)}",
+                    err=True,
+                )
+    elif issubclass(exc_type, NonZeroExit):
+        command, command_output = exc_value.args
+        click.echo(
+            f"[ERROR] Non zero return code while running command:\n{click.style(' '.join(command), fg='red', bold=True)}",
+            err=True,
+        )
+        click.echo(click.style(command_output, fg="red", italic=True), err=True)
+    else:
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+
+
+sys.excepthook = global_excepthook
