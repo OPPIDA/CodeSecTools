6bc31b0236641f720116a7d865bef9fc588a4720
Villon CHEN <villon.chen@oppida.fr>
2025-08-08 16:10:57 +0200

    mkdocs: dynamically generate SASTs and datasets docs from templates

---
+++ docs/gen_pages.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+import mkdocs_gen_files
+import yaml
+from jinja2 import Environment, FileSystemLoader
+
+DOCS_DIR = Path("docs")
+TEMPLATE_DIR = DOCS_DIR / "templates"
+SASTS_DIR = DOCS_DIR / "sasts"
+DATASETS_DIR = DOCS_DIR / "datasets"
+
+env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
+sasts_template = env.get_template("sasts.md.j2")
+datasets_template = env.get_template("datasets.md.j2")
+
+for sast in SASTS_DIR.glob("*.yaml"):
+    with mkdocs_gen_files.open(Path("sasts", sast.name), "r") as data_file:
+        sast_data = yaml.safe_load(data_file)
+
+    with mkdocs_gen_files.open(Path("sasts", f"{sast.stem}.md"), "w") as md_file:
+        md_file.write(sasts_template.render(sast_data))
+
+for dataset in DATASETS_DIR.glob("*.yaml"):
+    with mkdocs_gen_files.open(Path("datasets", dataset.name), "r") as data_file:
+        dataset_data = yaml.safe_load(data_file)
+
+    with mkdocs_gen_files.open(Path("datasets", f"{dataset.stem}.md"), "w") as md_file:
+        md_file.write(datasets_template.render(dataset_data))
