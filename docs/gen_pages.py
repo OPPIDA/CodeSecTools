f630094666b410a83b52fe77a28cc311b2aba2de
Villon CHEN <villon.chen@oppida.fr>
2025-08-09 15:58:19 +0200

    fix: supress ty unresolved-import for mkdocs_gen_files

--- docs/gen_pages.py
+++ docs/gen_pages.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
-import mkdocs_gen_files
 import yaml
 from jinja2 import Environment, FileSystemLoader
+from mkdocs_gen_files import open  # ty: ignore[unresolved-import]
 
 DOCS_DIR = Path("docs")
 TEMPLATE_DIR = DOCS_DIR / "templates"
@@ -14,21 +14,15 @@ sasts_template = env.get_template("sasts.md.j2")
 datasets_template = env.get_template("datasets.md.j2")
 
 for sast in SASTS_DIR.glob("*.yaml"):
-    with mkdocs_gen_files.open(Path("sasts", "data", sast.name), "r") as data_file:  # ty: ignore[unresolved-attribute]
+    with open(Path("sasts", "data", sast.name), "r") as data_file:
         sast_data = yaml.safe_load(data_file)
 
-    with mkdocs_gen_files.open(
-        Path("sasts", "data", f"{sast.stem}.md"), "w"
-    ) as md_file:  # ty: ignore[unresolved-attribute]
+    with open(Path("sasts", "data", f"{sast.stem}.md"), "w") as md_file:
         md_file.write(sasts_template.render(sast_data))
 
 for dataset in DATASETS_DIR.glob("*.yaml"):
-    with mkdocs_gen_files.open(
-        Path("datasets", "data", dataset.name), "r"
-    ) as data_file:  # ty: ignore[unresolved-attribute]
+    with open(Path("datasets", "data", dataset.name), "r") as data_file:
         dataset_data = yaml.safe_load(data_file)
 
-    with mkdocs_gen_files.open(
-        Path("datasets", "data", f"{dataset.stem}.md"), "w"
-    ) as md_file:  # ty: ignore[unresolved-attribute]
+    with open(Path("datasets", "data", f"{dataset.stem}.md"), "w") as md_file:
         md_file.write(datasets_template.render(dataset_data))
