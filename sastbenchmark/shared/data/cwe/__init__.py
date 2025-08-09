6faf155ba17ff4996009adb623860e46c8d3ccaf
Villon CHEN <villon.chen@oppida.fr>
2025-08-09 13:02:11 +0200

    Directories renaming

--- sastbenchmark/shared/data/cwe/__init__.py
+++ sastbenchmark/shared/data/cwe/__init__.py
@@ -3,7 +3,7 @@ import csv
 from sastbenchmark.utils import PACKAGE_DIR
 
 CWE = {}
-for file_path in (PACKAGE_DIR / "data" / "cwe").glob("CWE_*.csv"):
+for file_path in (PACKAGE_DIR / "shared" / "data" / "cwe").glob("CWE_*.csv"):
     with open(file_path, mode="r", encoding="utf-8") as file:
         reader = csv.DictReader(file)
         for row in reader:
