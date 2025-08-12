495d3f24b6486e7f259c825a5d57a1961d786a93
Villon CHEN <villon.chen@oppida.fr>
2025-08-12 14:44:21 +0200

    Rename data to profile and uniform sasts and datasets profiles template

--- docs/gen_pages.py
+++ docs/gen_pages.py
@@ -18,23 +18,22 @@ from mkdocs_gen_files import open  # ty: ignore[unresolved-import]
 
 DOCS_DIR = Path("docs")
 TEMPLATE_DIR = DOCS_DIR / "templates"
-SASTS_DIR = DOCS_DIR / "sasts" / "data"
-DATASETS_DIR = DOCS_DIR / "datasets" / "data"
+SASTS_DIR = DOCS_DIR / "sasts" / "profiles"
+DATASETS_DIR = DOCS_DIR / "datasets" / "profiles"
 
 env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
-sasts_template = env.get_template("sasts.md.j2")
-datasets_template = env.get_template("datasets.md.j2")
+profile_template = env.get_template("profile.md.j2")
 
 for sast in SASTS_DIR.glob("*.yaml"):
-    with open(Path("sasts", "data", sast.name), "r") as data_file:
+    with open(Path("sasts", "profiles", sast.name), "r") as data_file:
         sast_data = yaml.safe_load(data_file)
 
-    with open(Path("sasts", "data", f"{sast.stem}.md"), "w") as md_file:
-        md_file.write(sasts_template.render(sast_data))
+    with open(Path("sasts", "profiles", f"{sast.stem}.md"), "w") as md_file:
+        md_file.write(profile_template.render(sast_data))
 
 for dataset in DATASETS_DIR.glob("*.yaml"):
-    with open(Path("datasets", "data", dataset.name), "r") as data_file:
+    with open(Path("datasets", "profiles", dataset.name), "r") as data_file:
         dataset_data = yaml.safe_load(data_file)
 
-    with open(Path("datasets", "data", f"{dataset.stem}.md"), "w") as md_file:
-        md_file.write(datasets_template.render(dataset_data))
+    with open(Path("datasets", "profiles", f"{dataset.stem}.md"), "w") as md_file:
+        md_file.write(profile_template.render(dataset_data))
