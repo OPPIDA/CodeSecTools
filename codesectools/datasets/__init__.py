17eb56a9f1a7e6e913b6bf054e9a25363e9fc8c3
Villon CHEN <villon.chen@oppida.fr>
2025-08-13 16:05:43 +0200

    Ignore missing sasts and datasets during loading

--- codesectools/datasets/__init__.py
+++ codesectools/datasets/__init__.py
@@ -14,17 +14,21 @@ Attributes:
 import importlib
 
 from codesectools.datasets.core.dataset import Dataset
-from codesectools.utils import DATASETS_DIR
+from codesectools.utils import DATASETS_DIR, MissingFile
 
 DATASETS_ALL = {}
 for child in DATASETS_DIR.iterdir():
     if child.is_dir():
         if list(child.glob("dataset.py")) and child.name != "core":
-            dataset_name = child.name
+            try:
+                dataset_name = child.name
 
-            dataset_module = importlib.import_module(
-                f"codesectools.datasets.{dataset_name}.dataset"
-            )
-            dataset: Dataset = getattr(dataset_module, dataset_name)
+                dataset_module = importlib.import_module(
+                    f"codesectools.datasets.{dataset_name}.dataset"
+                )
+                dataset: Dataset = getattr(dataset_module, dataset_name)
 
-            DATASETS_ALL[dataset_name] = dataset
+                DATASETS_ALL[dataset_name] = dataset
+            except MissingFile:
+                # TODO: Print message
+                continue
--- codesectools/sasts/__init__.py
+++ codesectools/sasts/__init__.py
@@ -17,7 +17,7 @@ import importlib
 from click import Group
 
 from codesectools.sasts.core.sast import SAST, AnalysisResult
-from codesectools.utils import SASTS_DIR
+from codesectools.utils import SASTS_DIR, MissingFile
 
 SASTS_ALL = {}
 for child in SASTS_DIR.iterdir():
@@ -25,19 +25,26 @@ for child in SASTS_DIR.iterdir():
         if list(child.glob("sast.py")) and child.name != "core":
             sast_name = child.name
 
-            sast_module = importlib.import_module(
-                f"codesectools.sasts.{sast_name}.sast"
-            )
-            sast: SAST = getattr(sast_module, f"{sast_name}SAST")
-            analysis_result: AnalysisResult = getattr(
-                sast_module, f"{sast_name}AnalysisResult"
-            )
+            try:
+                sast_module = importlib.import_module(
+                    f"codesectools.sasts.{sast_name}.sast"
+                )
 
-            cli_module = importlib.import_module(f"codesectools.sasts.{sast_name}.cli")
-            cli: Group = getattr(cli_module, f"{sast_name}CLI")
+                sast: SAST = getattr(sast_module, f"{sast_name}SAST")
+                analysis_result: AnalysisResult = getattr(
+                    sast_module, f"{sast_name}AnalysisResult"
+                )
 
-            SASTS_ALL[sast_name] = {
-                "sast": sast,
-                "analysis_result": analysis_result,
-                "cli": cli,
-            }
+                cli_module = importlib.import_module(
+                    f"codesectools.sasts.{sast_name}.cli"
+                )
+                cli: Group = getattr(cli_module, f"{sast_name}CLI")
+
+                SASTS_ALL[sast_name] = {
+                    "sast": sast,
+                    "analysis_result": analysis_result,
+                    "cli": cli,
+                }
+            except MissingFile:
+                # TODO: Print message
+                continue
