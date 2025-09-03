3a82b0a5345cc44d15cf314e12d1727d65586905
Villon CHEN <villon.chen@oppida.fr>
2025-09-03 21:04:41 +0200

    Rename and move download test to cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,8 +1,11 @@
 """Test the main CLI commands."""
 
+import logging
+
 from typer.testing import CliRunner
 
-from codesectools.cli import cli
+from codesectools.cli import cli, get_downloadable
+from codesectools.datasets import DATASETS_ALL
 
 runner = CliRunner(env={"COLUMNS": "200"})
 
@@ -25,3 +28,21 @@ def test_status() -> None | AssertionError:
     """Test the 'status' command."""
     result = runner.invoke(cli, ["status"])
     assert result.exit_code == 0
+
+
+def test_download() -> None | AssertionError:
+    """Test the download of missing resources."""
+    logging.info("Downloading all missing resources")
+    result = runner.invoke(
+        cli, ["download", "all"], input="y\n" * len(get_downloadable())
+    )
+    assert result.exit_code == 0
+
+    for dataset_name, dataset in DATASETS_ALL.items():
+        assert dataset.is_cached()
+        for lang in dataset.supported_languages:
+            logging.info(f"Testing dataset {dataset_name} for {lang}")
+            dataset(lang=lang)
+
+    result = runner.invoke(cli, ["status", "--datasets"])
+    assert "❌" not in result.output
